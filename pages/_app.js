5029192500c7678c13fdddf0e8991f911bdc412a
Andrew A. Barber <hello@andrewbarber.me>
2020-10-28 16:30:40 +0000

    maybe this works

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,9 @@
-import '../styles/global.css'
+import "../styles/global.css";
+import { Amplify } from "aws-amplify";
+import awsExports from "../src/aws-exports";
+
+Amplify.configure({ ...awsExports, ssr: true });
 
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+	return <Component {...pageProps} />;
 }
